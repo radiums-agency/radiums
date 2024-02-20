09e55c86f905e44c47cab130035136d45aad5c38
radiums-agency <radiumsagency@gmail.com>
2024-02-20 16:02:33 +0000

    static

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -14,11 +14,11 @@ const config = {
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
-			adapter: adapter({
-				runtime: 'nodejs20.x',
-				regions: ['fra1', 'cdg1'],
-			}),
-			
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
 		})
 	}
 };
