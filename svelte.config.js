538b1c9d020998001a9c2b97086455ba9c32e1fd
radiums-agency <radiumsagency@gmail.com>
2024-02-20 15:54:42 +0000

    region settings

--- svelte.config.js
+++ svelte.config.js
@@ -14,6 +14,10 @@ const config = {
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
+			adapter: adapter({
+				runtime: 'nodejs20.x',
+				regions: ['fra1', 'cdg1'],
+			}),
 			
 		})
 	}
